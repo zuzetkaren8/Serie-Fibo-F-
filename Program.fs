5ab3c4a874ae999cc760c6d2891f16064a0c383c
Zuzet Karen Sanga Mamani <zuzetkaren8@gmail.com>
2022-06-15 22:14:43 -0400

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,29 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+let fib5 n = 
+    let mutable last = 0
+    let mutable next = 1
+    let mutable sigu = 1
+    seq {
+        0
+        1
+        1
+        for i in 1 .. (n-3) do
+            let temp = last + next + sigu
+            last <- next
+            next <- sigu
+            sigu <- temp
+            sigu
+      }
+
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Introduce la cantidad de numeros que deseas de la secuencia de fibonacci")
+    let valu = Console.ReadLine()
+    let m=valu |> int
+    Console.Write(m)
+    Console.WriteLine("Secuencia Fibonacci:")
+    let run = fib5 m|> Seq.iter (printf "%d,")
+    Console.WriteLine(run)
+    0
+  
